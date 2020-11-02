9211c6e2357120cf5ee1e711fd801299c7ce7d31
Anthony Fu <anthonyfu117@hotmail.com>
2020-11-02 16:46:35 +0800

    pref: improve pref

--- README.md
+++ README.md
@@ -4,14 +4,14 @@ My new plugin for the TypeScript playground.
 
 ## Running this plugin
 
-- [Click this link](https://www.staging-typescript.org/play?install-plugin=[name]) to install
+- [Click this link](https://www.staging-typescript.org/play?install-plugin=@type-challenges/playground-plugin) to install
 
 or
 
 - Open up the TypeScript Playground
 - Go the "Plugins" in the sidebar
 - Look for "Plugins from npm"
-- Add "[name]"
+- Add `@type-challenges/playground-plugin`
 - Reload the browser
 
 Then it will show up as a tab in the sidebar.
--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,27 @@ import type { Sandbox } from './vendor/sandbox'
 const log = (...args) => console.log('%ctype-challenges =>', 'color: yellow', ...args)
 
 const makePlugin = (utils: PluginUtils) => {
-  let mainDiv: HTMLDivElement | null = null
-  let showTypesContainer: HTMLElement | null = null
+  let mainDiv: HTMLDivElement
+  let showTypesContainer: HTMLElement
+  let program: ts.Program
+  let typeChecker: ts.TypeChecker
+  let _initPromise: Promise<void> | undefined
+
+  async function init(sandbox: Sandbox) {
+    if (!_initPromise) {
+      _initPromise = (async()=>{
+        program = await sandbox.createTSProgram()
+        typeChecker = program.getTypeChecker()
+      })()
+    }
+
+    return _initPromise
+  }
 
   async function getShowTypesInCode (sandbox: Sandbox, model: editor.ITextModel) {
+    await init(sandbox)
+    
     const startTime = window.performance.now()
-    const program = await sandbox.createTSProgram()
     const sourceFile = program.getSourceFile(sandbox.filepath)
     const needShowTypes = sourceFile.statements.filter((stat) => {
       if (sandbox.ts.isTypeAliasDeclaration(stat)) {
@@ -23,18 +38,14 @@ const makePlugin = (utils: PluginUtils) => {
       }
       return false
     }) as ts.TypeAliasDeclaration[]
-    const typeChecker = program.getTypeChecker()
     const result = needShowTypes.map((el) => {
       const displayParts = (sandbox.ts as any).typeToDisplayParts(typeChecker, typeChecker.getTypeAtLocation(el), undefined, sandbox.ts.TypeFormatFlags.InTypeAlias)
       const typeString = sandbox.ts.displayPartsToString(displayParts)
       return `type ${el.name.getText()} = ${typeString}`
     }).join('\n')
 
-    if (!showTypesContainer) {
-      showTypesContainer = utils.createDesignSystem(mainDiv).code(result)
-    } else {
+    
       showTypesContainer.textContent = result
-    }
     log(`getShowTypesInCode - ${window.performance.now() - startTime}ms`)
   }
 
@@ -43,10 +54,12 @@ const makePlugin = (utils: PluginUtils) => {
     displayName: "Type Challenges",
     didMount: (sandbox, container) => {
       log("DidMount type-challenges plugin")
+      init(sandbox)
 
       // Create a design system object to handle
       // making DOM elements which fit the playground (and handle mobile/light/dark etc)
       mainDiv = container
+      showTypesContainer = utils.createDesignSystem(mainDiv).code('')
     },
 
     // This is called occasionally as text changes in monaco,
