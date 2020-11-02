1247381d579abc23def3f570fb52ac1cead63f44
Anthony Fu <anthonyfu117@hotmail.com>
2020-11-02 17:06:22 +0800

    feat: colorize

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import type { editor } from 'monaco-editor'
 import type ts from 'typescript'
-import type { PlaygroundPlugin, PluginUtils } from "./vendor/playground"
+import type { PlaygroundPlugin, PluginUtils } from './vendor/playground'
 import type { Sandbox } from './vendor/sandbox'
 
-const log = (...args) => console.log('%ctype-challenges =>', 'color: yellow', ...args)
+const log = (...args: any) => console.log('%ctype-challenges =>', 'color: teal', ...args)
 
 const makePlugin = (utils: PluginUtils) => {
   let mainDiv: HTMLDivElement
@@ -14,7 +14,7 @@ const makePlugin = (utils: PluginUtils) => {
 
   async function init(sandbox: Sandbox) {
     if (!_initPromise) {
-      _initPromise = (async()=>{
+      _initPromise = (async () => {
         program = await sandbox.createTSProgram()
         typeChecker = program.getTypeChecker()
       })()
@@ -23,14 +23,15 @@ const makePlugin = (utils: PluginUtils) => {
     return _initPromise
   }
 
-  async function getShowTypesInCode (sandbox: Sandbox, model: editor.ITextModel) {
+  async function getShowTypesInCode(sandbox: Sandbox, model: editor.ITextModel) {
     await init(sandbox)
-    
+
+    const ts = sandbox.ts
     const startTime = window.performance.now()
-    const sourceFile = program.getSourceFile(sandbox.filepath)
+    const sourceFile = program.getSourceFile(sandbox.filepath)!
     const needShowTypes = sourceFile.statements.filter((stat) => {
-      if (sandbox.ts.isTypeAliasDeclaration(stat)) {
-        const leadingComments = sandbox.ts.getLeadingCommentRanges(sourceFile.getFullText(), stat.pos) || []
+      if (ts.isTypeAliasDeclaration(stat)) {
+        const leadingComments = ts.getLeadingCommentRanges(sourceFile.getFullText(), stat.pos) || []
         return leadingComments.some((comment) => {
           const text = sourceFile.getFullText().slice(comment.pos, comment.end)
           return text.includes('@show-types')
@@ -38,28 +39,33 @@ const makePlugin = (utils: PluginUtils) => {
       }
       return false
     }) as ts.TypeAliasDeclaration[]
-    const result = needShowTypes.map((el) => {
-      const displayParts = (sandbox.ts as any).typeToDisplayParts(typeChecker, typeChecker.getTypeAtLocation(el), undefined, sandbox.ts.TypeFormatFlags.InTypeAlias)
-      const typeString = sandbox.ts.displayPartsToString(displayParts)
-      return `type ${el.name.getText()} = ${typeString}`
-    }).join('\n')
 
-    
-      showTypesContainer.textContent = result
+    const result = needShowTypes
+      .map((el) => {
+        // @ts-expect-error - private API
+        const displayParts = ts.typeToDisplayParts(typeChecker, typeChecker.getTypeAtLocation(el), undefined, sandbox.ts.TypeFormatFlags.InTypeAlias)
+        const typeString = ts.displayPartsToString(displayParts)
+        return `type ${el.name.getText()} = ${typeString}`
+      })
+      .join('\n')
+
+    showTypesContainer.innerHTML = await sandbox.monaco.editor.colorize(result, 'typescript', {})
     log(`getShowTypesInCode - ${window.performance.now() - startTime}ms`)
   }
 
   const customPlugin: PlaygroundPlugin = {
-    id: "type-challenges",
-    displayName: "Type Challenges",
+    id: 'type-challenges',
+    displayName: 'Type Challenges',
     didMount: (sandbox, container) => {
-      log("DidMount type-challenges plugin")
+      log('DidMount type-challenges plugin')
       init(sandbox)
 
       // Create a design system object to handle
       // making DOM elements which fit the playground (and handle mobile/light/dark etc)
       mainDiv = container
       showTypesContainer = utils.createDesignSystem(mainDiv).code('')
+      const pre = showTypesContainer.parentElement!
+      pre.setAttribute('lang', 'ts')
     },
 
     // This is called occasionally as text changes in monaco,
@@ -75,7 +81,7 @@ const makePlugin = (utils: PluginUtils) => {
     // Gives you a chance to remove anything set up,
     // the container itself if wiped of children after this.
     didUnmount: () => {
-      log("De-focusing plugin")
+      log('De-focusing plugin')
     },
   }
 
