361d3c2aeaac17760ca865681f3488591acebc6b
Pig Fang <g-plane@hotmail.com>
2020-11-19 10:42:40 +0800

    more refactoring with Svelte

---
+++ src/components/App.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import type { Sandbox } from '../vendor/sandbox'
+  import { onMount } from 'svelte'
+  import Challenge from './Challenge.svelte'
+  import Types from './Types.svelte'
+
+  export let tabBar: HTMLDivElement
+  export let sandbox: Sandbox
+
+  let active = 'challenge'
+  let tabs: { name: string; text: string }[] = [
+    { name: 'challenge', text: 'Challenge' },
+    { name: 'types', text: 'Types' },
+  ]
+
+  let tabsRef: HTMLElement
+
+  onMount(() => {
+    Array.from(tabsRef.children).forEach((tab) => tabBar.appendChild(tab))
+  })
+</script>
+
+<style>
+  section,
+  .hide {
+    display: none;
+  }
+</style>
+
+<section bind:this={tabsRef}>
+  {#each tabs as { name, text } (name)}
+    <button
+      role="tab"
+      class:active={active === name}
+      on:click={() => (active = name)}>
+      {text}
+    </button>
+  {/each}
+</section>
+
+<!-- We don't use `if` directive here because of keeping components alive. -->
+<div class:hide={active !== 'challenge'}>
+  <Challenge {sandbox} />
+</div>
+<div class:hide={active !== 'types'}>
+  <Types {sandbox} />
+</div>
--- src/index.ts
+++ src/index.ts
@@ -1,67 +1,19 @@
 import type { PlaygroundPlugin, PluginUtils } from './vendor/playground'
-import Challenge from './components/Challenge.svelte'
-import Types from './components/Types.svelte'
+import App from './components/App.svelte'
 
 const log = (...args: any) => console.log('%ctype-challenges =>', 'color: teal', ...args)
 
-const tabsDefine = {
-  challenge: 'Challenge',
-  types: 'Types',
-}
-
 const makePlugin = (utils: PluginUtils) => {
-  let tabBar: HTMLDivElement
-  let tabs: Record<keyof typeof tabsDefine, { name: string; button: HTMLButtonElement; panel: HTMLDivElement }> = {} as any
-  let activeTab: keyof typeof tabsDefine = 'challenge'
-
-  let ds: ReturnType<typeof utils.createDesignSystem>
-
   const customPlugin: PlaygroundPlugin = {
     id: 'type-challenges',
     displayName: 'Type Challenges',
     didMount: (sandbox, container) => {
       log('DidMount type-challenges plugin')
-      ds = utils.createDesignSystem(container)
+      const ds = utils.createDesignSystem(container)
       container.id = 'type-challenges'
 
-      tabBar = ds.createTabBar()
-
-      const panels = document.createElement('div')
-
-      ;(Object.entries(tabsDefine) as [keyof typeof tabsDefine, string][]).map(([key, name]) => {
-        const button = ds.createTabButton(name)
-        const panel = document.createElement('div')
-
-        button.onclick = () => {
-          activeTab = key
-          Object.values(tabs).forEach((i) => {
-            if (i.button === button) {
-              i.button.classList.add('active')
-              panels.appendChild(i.panel)
-            } else {
-              i.button.classList.remove('active')
-              panels.removeChild(i.panel)
-            }
-          })
-        }
-        tabBar.append(button)
-
-        if (key === activeTab) {
-          panels.append(panel)
-          button.classList.add('active')
-        }
-
-        tabs[key] = {
-          name,
-          button,
-          panel,
-        }
-      })
-      container.appendChild(tabBar)
-      container.appendChild(panels)
-
-      new Challenge({ target: tabs.challenge.panel, props: { sandbox } })
-      new Types({ target: tabs.types.panel, props: { sandbox } })
+      const tabBar = ds.createTabBar()
+      new App({ target: container, props: { tabBar, sandbox } })
     },
 
     modelChangedDebounce: async () => {
