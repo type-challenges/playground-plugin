b8dc614740784ae956523a30a744ccf4a8a6ea10
Curly Brackets <water.curly@outlook.com>
2021-12-06 23:41:11 +0800

    fix: copy locales & remove unused check

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,36 @@
-import type { PlaygroundPlugin, PluginUtils } from './vendor/playground'
-import { VirtualTypeScriptEnvironment } from './vendor/typescript-vfs'
-import { log } from './logger'
-import App from './components/App.svelte'
-import { prepareTSVfs } from './typeHelpers'
-import { Context } from './types'
+import type { PlaygroundPlugin, PluginUtils } from "./vendor/playground";
+import { VirtualTypeScriptEnvironment } from "./vendor/typescript-vfs";
+import { log } from "./logger";
+import App from "./components/App.svelte";
+import { prepareTSVfs } from "./typeHelpers";
+import { Context } from "./types";
 
 export default function (utils: PluginUtils) {
-  let vfs: VirtualTypeScriptEnvironment
+  let vfs: VirtualTypeScriptEnvironment;
 
   const customPlugin: PlaygroundPlugin = {
-    id: 'type-challenges',
-    displayName: 'Type Challenges',
+    id: "type-challenges",
+    displayName: "Type Challenges",
     didMount: async (sandbox, container) => {
-      log('DidMount type-challenges plugin')
-      container.id = 'type-challenges'
-      vfs = await prepareTSVfs(sandbox)
+      log("DidMount type-challenges plugin");
+      container.id = "type-challenges";
+      vfs = await prepareTSVfs(sandbox);
 
-      const context: Context = { sandbox, vfs }
-      new App({ target: container, props: { context } })
-
-      sandbox.setCompilerSettings({
-        noUnusedLocals: false,
-        noUnusedParameters: false,
-      })
+      const context: Context = { sandbox, vfs };
+      new App({ target: container, props: { context } });
     },
 
     modelChangedDebounce: (sandbox) => {
-      log('modelChangedDebounce in type-challenges')
+      log("modelChangedDebounce in type-challenges");
 
-      vfs?.updateFile(sandbox.filepath, sandbox.getText())
-      window.dispatchEvent(new CustomEvent('codeChanged'))
+      vfs?.updateFile(sandbox.filepath, sandbox.getText());
+      window.dispatchEvent(new CustomEvent("codeChanged"));
     },
 
     didUnmount: () => {
-      log('De-focusing plugin')
+      log("De-focusing plugin");
     },
-  }
+  };
 
-  return customPlugin
+  return customPlugin;
 }
